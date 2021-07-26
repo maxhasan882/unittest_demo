21be86919595163eca98454a721f207f3e6a9c77
Hasan <rhmithu50@gmail.com>
2021-07-26 14:38:52 +0600

    initial

---
+++ polls/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+class DemoTests(TestCase):
+    def setUp(self):
+        self.actual = 1
+        self.expected = 1
+
+    def test_actual_expected(self):
+        self.assertEqual(self.expected, self.actual)
