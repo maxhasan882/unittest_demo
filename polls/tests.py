08024fbc7333158572be75510342baa6072f0fdb
Hasan <rhmithu50@gmail.com>
2021-07-26 15:15:02 +0600

    Test

--- polls/tests.py
+++ polls/tests.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 class DemoTests(TestCase):
     def setUp(self):
         self.actual = 1
-        self.expected = 1
+        self.expected = 2
 
     def test_actual_expected(self):
         self.assertEqual(self.expected, self.actual)
